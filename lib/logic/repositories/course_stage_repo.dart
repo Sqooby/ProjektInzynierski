d65fe8d35c0331c1f0828a534329ee944af1f8bd
Sqooby <michal.basznianin3@gmail.com>
2023-09-24 18:51:31 +0200

    Added courseStage repo

---
+++ lib/logic/repositories/course_stage_repo.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:pv_analizer/logic/models/course_stage_list.dart';
+
+import 'package:http/http.dart' as http;
+
+class CourseStageRepo {
+  final String url = 'https://demo1.drt.kia.prz.edu.pl/api/CourseStage/';
+  Future<List<CourseStageList>> getCourseStage() async {
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    print(response);
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as List;
+
+      final result = json.map((e) {
+        return CourseStageList(
+          id_bus_stop: e['id_bus_stop'],
+          arr_time: e['arr_time'],
+          charging_time: e['charging_time'],
+          dep_time: e['dep_time'],
+          id_course: e['id_course'],
+          id_course_stage: e['id_course_stage'],
+          stage: e['stage'],
+        );
+      }).toList();
+
+      return result;
+    } else {
+      throw '${response.statusCode}';
+    }
+  }
+}
