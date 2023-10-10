c673bd9f00a3028cbea058f1983213c31e84df29
Sqooby <michal.basznianin3@gmail.com>
2023-10-10 14:04:04 +0200

    added try catche method

---
+++ lib/screens/BusStop/bus_stop_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pv_analizer/screens/BusStop/bus_stop_body.dart';
+
+class BusStopScreen extends StatelessWidget {
+  const BusStopScreen({Key? key}) : super(key: key);
+  static const String route = '/busStop';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(child: BusStopBody()),
+    );
+  }
+}
